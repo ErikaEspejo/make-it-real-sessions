002c1acbdc01173296cb896c4026495c2280b9cc
Erika Espejo <erikaespejo95@gmail.com>
2021-04-04 10:14:23 -0500

    variables

---
+++ 01-variable/app.js
@@ -0,0 +1,29 @@
+//console.log("hola mundo")
+
+// cadenas de texto
+console.log('Jesus 123');
+console.log('12345');
+console.log('5' + '1'); //concatenacion
+console.log('Jesus' + ' ' + 'Estrada');
+console.log('El resultado de 5 + 3 es: ' + 5 + 3); //convierte los numeros en texto y concatena '53'
+console.log('El resultado de 5 + 3 es: ' + (5 + 3)); //mantiene su tipo, concatena '8'
+
+
+//numero
+console.log(5);
+console.log(1 + 5 * 3); //precedencia de operadores
+console.log((1 + 5) * 3);
+console.log(0 / 1);
+console.log(1 / 0); //Infinity
+
+//booleanos
+
+console.log(true);
+console.log(false);
+console.log(!true);
+console.log(!false);
+console.log(5 > 3);
+console.log(!(5 > 3));
+console.log('1' == 1);
+console.log('1' === 1);
+
---
+++ 01-variable/variables.js
@@ -0,0 +1,5 @@
+var variable1; //se puede reasignar --no se recomienda
+let variable2; //se puede reasignar
+const variable3 = 1; //no se puede reasignar --se debe inicializar
+
+typeof variable3; //muestra el tipo de dato de una variable

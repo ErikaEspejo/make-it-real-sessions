b16105d27db5fd2ce5dfd721c5aa6acd3b7dd497
Erika Espejo <erikaespejo95@gmail.com>
2021-04-04 11:35:55 -0500

    modulos - exportacion de una funcion

---
+++ 07-node-app/encriptar.js
@@ -0,0 +1,27 @@
+/*
+  MODULOS
+
+  Con la creación de modulos se empiezan a separar responsabilidades dentro de una aplicación de node.
+  La idea es crear tantos modulos como se requiera, y que estos solo sean llamados desde el archivos principal de la app,
+  en este caso index.js
+
+  De manera que el archivo principal solo cumple la responsabilidad de llamar los modulos, pero cada uno se ejecuta
+  por fuera de este archivo y el codigo se vuelve escalable y mantenible.
+
+*/
+
+
+
+const bcrypt = require('bcrypt'); //busca el modulo bcrypt y lo aloja en la variable bcrypt
+
+const encriptar = (cadena) => { //Dentro de la funcion queda todo lo que solo se requiere para la funcion para evitar alterar su contexto
+
+  const saltRounds = 10; //numero de veces que voltea o encripta un texto
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(cadena, salt); //Encriptacion
+
+  return hash;
+}
+
+
+module.exports = encriptar; //exporto la funcion de manera que cualquier archivo pueda consumirla
--- 07-node-app/index.js
+++ 07-node-app/index.js
@@ -2,15 +2,13 @@
 //process.argv permite ingresar datos por consola, donde cada palabra hace parte de un arreglo, [node,index.js,palabra1,....]; por lo cual para hacer algo con estos datos se debe acceder como si fuera un areglo por su posicion, 2 en adelante.
 
 //Para capturar argumentos de entrada al llamar la app.
+
+const encriptar = require("./encriptar"); //importar el modulo encriptar mediante su ruta y se debe alojar en una variable
+
 const argumentos = process.argv;
 const cadenaEntrada = argumentos.slice(2).join(' ');
 
-
-const bcrypt = require('bcrypt'); //busca el modulo bcrypt y lo aloja en la variable bcrypt
-const saltRounds = 10; //numero de veces que voltea o encripta un texto
-
-const salt = bcrypt.genSaltSync(saltRounds);
-const hash = bcrypt.hashSync(cadenaEntrada, salt); //Encriptacion
+const hash = encriptar(cadenaEntrada); //se hace uso del modulo
 
 console.log(cadenaEntrada);
 console.log(hash);

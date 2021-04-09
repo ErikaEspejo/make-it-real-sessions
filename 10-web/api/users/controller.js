e02e86462d3d3e36a8cfcc4955a31c82a6cc341d
Erika Espejo <erikaespejo95@gmail.com>
2021-04-09 18:46:09 -0500

    actualizar usuario, crear usuario y obtener usuario - metodos put y get

--- 10-web/api/users/controller.js
+++ 10-web/api/users/controller.js
@@ -4,5 +4,57 @@ const list = (req, res) => {
   res.json(users);
 };
 
+const create = (req, res) => {
+  const { name, email, username, password } = req.body; //Destructuración de las llaves - valor del request
+  if (name && email && username && password) { //Si se incluyen todos los datos intenta guardarlo, si no genera error.
+    const user = {
+      name,
+      email,
+      username,
+      password,
+    };
 
-module.exports = { list };
+    const found = users.filter((u) => u.username === user.username);
+    if (found && found.length > 0) { //Si se incluyen todos los datos pero se encuentra otro usuario con el mismo username, manda un mensaje de que ya existe, si no lo crea.
+      res.json({ message: 'Ya existe el usuario.' })
+    } else {
+      users.push(user);
+      res.json(users); //muestra el json del listado de usuarios.
+    };
+
+  } else {
+    res.json({ message: 'Ocurrió un error. Hay datos nulos.' })
+  };
+};
+
+const getUser = (req, res) => {
+  const username = req.params.username; //entra a los parametros del request (username)
+  const user = users.filter(user => user.username === username); //filtra solo el usuario
+
+  if (Object.keys(user).length === 0) {
+    res.send('Usuario no encontrado');
+  } else {
+    const name = user[0].name;
+    const email = user[0].email;
+
+    res.send(`Username: ${username}, Nombre: ${name}, Email: ${email}`);
+  };
+};
+
+const update = (req, res) => {
+  const username = req.params.username;
+  const found = users.filter((u) => u.username === username);
+
+  if (found && found.length > 0) { //Si se incluyen todos los datos pero se encuentra otro usuario con el mismo username, actualiza con los datos ingresados.
+
+    found[0].name = req.body.name ? req.body.name : found[0].name;
+    found[0].email = req.body.email ? req.body.email : found[0].email;
+    found[0].password = req.body.password ? req.body.password : found[0].password;
+
+    res.json(users);
+  } else {
+    res.json({ message: `El usuario ${username} no existe.` }); //muestra que el usuario no existe
+  };
+};
+
+module.exports = { list, create, getUser, update };
--- 10-web/api/users/router.js
+++ 10-web/api/users/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { list } = require('./controller');
+const { list, create, getUser, update } = require('./controller');
 const { logger } = require('./../middleware/logger');
 const router = express.Router();
 
@@ -7,5 +7,10 @@ router.use(logger);
 
 router.route('/')
   .get(list)
+  .post(create)
+
+router.route('/:username')
+  .get(getUser)
+  .put(update)
 
 module.exports = router;

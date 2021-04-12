2fec4b847f7dc1be90380c981e409518b188997c
Erika Espejo <erikaespejo95@gmail.com>
2021-04-12 11:40:57 -0500

    helmet - security

--- 11-web/api/index.js
+++ 11-web/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan'); //log predeterminado
 const fs = require('fs'); //se usa con morgan para crear logs en archivos
 const path = require('path'); //se usa con morgan para crear logs en archivos
+const helmet = require('helmet');
 const { localization } = require('./middleware/localization');
 
 const users = require('./users/router');
@@ -14,6 +15,8 @@ const router = express.Router();
 const accessLogDir = config.logs.access; //se crea la direccion donde se guarda el log en el archivo config, y directamente en las variables de entorno
 
 const logStream = fs.createWriteStream(path.join(__dirname, accessLogDir), { flags: 'a' }); //crear logs en archivos
+
+router.use(helmet);
 router.use(morgan('combined', { stream: logStream }));
 router.use(localization);
 

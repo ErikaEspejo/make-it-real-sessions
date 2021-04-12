78b73867309e974dfd93b5e175e604ea4144de07
Erika Espejo <erikaespejo95@gmail.com>
2021-04-11 22:56:18 -0500

    async await

--- 10-promises.js/promises.js
+++ 10-promises.js/promises.js
@@ -38,12 +38,31 @@ const getMovies = () => {
 };
 
 //then && catch
-
+/*
 getMovies()//se llama la funcion
   .then((movies) => { //en este then se ejecuta el codigo de lo que se quiere hacer con la informacón una vez que se obtiene. Data, o cualquier nombre funciona
     movies.map(movie => console.log(movie.title));
   })
   .catch((error) => {console.log(error)}); //igual que el then pero capta el error
+*/
 
-const moviesList = getMovies();
-console.log(moviesList);
+//async await
+// espera a que se cumpla una promesa para continuar.
+//Para usarla se debe hacer dentro de una funcion declarada como asincrona y puede llamar cualquier funcion
+
+
+const load = async () => { //declaracion de funcion asincrona
+  try {
+    const data = await getMovies(); //esta asignando a una variable la salida de la respuesta
+    //si la promesa await no se termina de resolver el siguiente codigo no se ejecuta. Se resuelve el resolve dentro de la funcion getMovies
+    data.map(d => console.log(d.title));
+  } catch(error) {
+  console.log(error);
+  };
+};
+
+//Para manejo de errores dentro de un async await se hace uso de try catch, try para el resolve y catch para el reject
+//Es mas legible usar el async await que el then catch.
+
+load();
+
